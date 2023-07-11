a95d8bcdc939679ea3ed071fa9b3bbdad59211c6
Aman0246 <amankashyap0246@gmail.com>
2023-07-11 11:22:39 +0530

    messagemodel work compleated

--- Backend/Routes/Routes.js
+++ Backend/Routes/Routes.js
@@ -2,6 +2,7 @@ const express=require("express")
 const routes=express.Router()
 const {adduser,login,user,alluser}=require("../Controllers/user")
 const {newconversation} =require("../Controllers/conversation-controller")
+const {newMessage,getMessage}=require("../Controllers/messageController")
 
 //registration
 routes.post("/adduser",adduser)
@@ -9,6 +10,8 @@ routes.post("/login",login)
 routes.post("/userCheck",user)
 routes.get("/alluser",alluser)
 routes.post("/conversation/add",newconversation)
+routes.post("/newMessage",newMessage)
+routes.post("/allmessage/:id",getMessage)
 
 
 module.exports={routes}
